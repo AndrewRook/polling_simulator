f1ca8dbc981cd71542054c0054cf4c4009188f06
Andrew Schechtman-Rook <rook166@gmail.com>
2020-11-21 10:39:24 -0500

    updating to allow for multiple attempts

--- polling_simulator/strategies.py
+++ polling_simulator/strategies.py
@@ -1,30 +1,7 @@
 import numpy as np
 import pandas as pd
 
-
-def naive_sample_predefined():
-    """ie "I'm going to call 1000 people at total random, and we'll just
-    use those responses as if they were representative"
-    """
-
-
-def naive_sample_guaranteed():
-    """ie "I'm going to call people at random until I get 1000 responses,
-    and we'll just use those responses as if they were representative"
-    """
-
-
-def pre_stratified_sample_predefined():
-    """ie "I'm going to call 1000 people randomly selected to be based on
-    assumed demographic representation, and we'll use those as representative"
-    """
-
-
-def pre_stratified_sample_guaranteed():
-    """ie "I'm going to call people at random, selected based on assumed demographic
-    representation, until I get 1000 responses, and we'll use those as representative"
-    """
-
+from typing import Union, Tuple
 
 def post_stratified_sample_predefined():
     """ie "I'm going to call 1000 people at random, and then we'll weight the responses
@@ -38,17 +15,18 @@ def post_stratified_sample_guaranteed():
     """
 
 
-def predefined_sample(n_sample, shuffled_electorate, demographics):
+def predefined_sample(n_sample, shuffled_electorate, max_num_attempts):
     """ie I'm going to call 1000 people at random, and just use those who answer"""
     if n_sample > len(shuffled_electorate):
         raise ValueError(f"number of samples ({n_sample}) greater than electorate ({len(shuffled_electorate)})")
     people_called = shuffled_electorate.head(n_sample)
     does_respond = people_called["response_likelihood"] > np.random.random(len(people_called))
     poll_responders = people_called[does_respond].reset_index(drop=True)
-    return poll_responders
+    poll_non_responders = people_called[does_respond == False].reset_index(drop=True)
+    return poll_responders, poll_non_responders
 
 
-def guaranteed_sample(n_sample, shuffled_electorate, demographics):
+def guaranteed_sample(n_sample, shuffled_electorate, max_num_attempts):
     """ie I'm going to call people at random until I get 1000 responses"""
     does_respond = shuffled_electorate["response_likelihood"] > np.random.random(len(shuffled_electorate))
     poll_responders = shuffled_electorate[does_respond]
@@ -56,10 +34,30 @@ def guaranteed_sample(n_sample, shuffled_electorate, demographics):
         raise ValueError(
             f"number of samples ({n_sample}) greater than number of poll responders ({len(poll_responders)})"
         )
-    return poll_responders.head(n_sample)
+    return poll_responders.head(n_sample), None
 
 
-def predefined_sample_pre_stratified(n_sample, shuffled_electorate, demographics):
+def _get_responses(
+        response_likelihoods: Union[pd.Series, np.float64], max_num_attempts: int
+):
+    # -> Tuple[np.ndarray[np.bool_], np.ndarray[np.int64]]: # Not working, not sure why
+    """Figure out whether or not someone responds after N attempts. Return both whether or
+    not they responded but also the number of attempts made."""
+    if max_num_attempts < 1:
+        raise ValueError("max_num_attempts must be a positive integer")
+    realized_response_matrix = np.random.random((max_num_attempts, len(response_likelihoods)))
+    if isinstance(response_likelihoods, pd.Series):
+        response_likelihoods = response_likelihoods.values
+
+    did_respond_matrix = realized_response_matrix < response_likelihoods
+    did_respond = did_respond_matrix.any(axis=0)
+    num_attempts_required = did_respond_matrix.argmax(axis=0) + 1
+    num_attempts_required[did_respond == False] = -1
+    return did_respond, num_attempts_required
+
+
+
+def predefined_sample_pre_stratified(n_sample, shuffled_electorate, demographics, max_num_attempts):
     """ie I'm going to call 1000 people, randomly selected based on assumed demographic representation"""
     n_sample_per_demo = [
         round(n_sample * demographic.population_percentage)
@@ -76,7 +74,7 @@ def predefined_sample_pre_stratified(n_sample, shuffled_electorate, demographics
     return pd.concat(poll_responders).reset_index(drop=True)
 
 
-def guaranteed_sample_pre_stratified(n_sample, shuffled_electorate, demographics):
+def guaranteed_sample_pre_stratified(n_sample, shuffled_electorate, demographics, max_num_attempts):
     """ie I'm going to call people at random, selected based on assumed demographic representation,
     until I get 1000 responses"""
     n_sample_per_demo = [
---
+++ tests/test_strategies.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from polling_simulator import strategies
+
+# @pytest.fixture(scope="function")
+# def electorate():
+#     data = pd.DataFrame({
+#         "response_likelihood": [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+#     })
+
+
+class TestPredefinedSample:
+    def test_fails_when_asked_for_too_many_people(self):
+        data = pd.DataFrame({
+            "response_likelihood": np.ones(10)
+        })
+        with pytest.raises(ValueError):
+            strategies.predefined_sample(20, data, 1)
+
+    def test_returns_fewer_than_desired_when_response_rates_are_low(self):
+        np.random.seed(123)
+        data = pd.DataFrame({
+            "response_likelihood": np.ones(20) * 0.5
+        })
+        poll_responders, poll_non_responders = strategies.predefined_sample(10, data, 1)
+        assert len(poll_responders) < 10
+        assert len(poll_responders) + len(poll_non_responders) == 10
+
+    def test_reaches_more_people_when_makes_multiple_attempts(self):
+        np.random.seed(123)
+        data = pd.DataFrame({
+            "response_likelihood": np.ones(40) * 0.5
+        })
+        single_call_responders, _ = strategies.predefined_sample(20, data, 1)
+        multi_call_responders, non_responders = strategies.predefined_sample(20, data, 5)
+        assert len(multi_call_responders) > len(single_call_responders)
+        assert len(multi_call_responders) < 20
+        assert len(multi_call_responders) + len(non_responders) == 20
+
+
+class TestInternalGetResponses:
+    def test_fails_when_passed_zero_attempts(self):
+        with pytest.raises(ValueError):
+            strategies._get_responses(None, 0)
+
+    def test_works_with_single_attempt(self):
+        np.random.seed(123)
+        response_likelihoods = np.ones(40) * 0.5
+        did_respond, num_attempts_required = strategies._get_responses(response_likelihoods, 1)
+        assert np.sum(did_respond) < len(response_likelihoods)
+        np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1]))
+        assert np.min(num_attempts_required[did_respond]) == 1
+
+    def test_works_with_pandas_series(self):
+        np.random.seed(123)
+        response_likelihoods = pd.Series(np.ones(40) * 0.5)
+        did_respond, num_attempts_required = strategies._get_responses(response_likelihoods, 1)
+        assert np.sum(did_respond) < len(response_likelihoods)
+        np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1]))
+        assert np.min(num_attempts_required[did_respond]) == 1
+
+    def test_works_with_multiple_attempts(self):
+        np.random.seed(123)
+        response_likelihoods = np.ones(40) * 0.25
+        did_respond_single, _ = strategies._get_responses(response_likelihoods, 1)
+        did_respond_multi, num_attempts_required = strategies._get_responses(response_likelihoods, 3)
+        assert np.sum(did_respond_multi) > np.sum(did_respond_single)
+        np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1, 2, 3]))
+        assert np.min(num_attempts_required[did_respond_multi]) == 1
+
+
+
+class TestGuaranteedSample:
+    def test_fails_when_asked_for_too_many_people(self):
+        np.random.seed(123)
+        data = pd.DataFrame({
+            "response_likelihood": np.ones(10) * 0.05
+        })
+        with pytest.raises(ValueError):
+            strategies.guaranteed_sample(5, data, 1)
