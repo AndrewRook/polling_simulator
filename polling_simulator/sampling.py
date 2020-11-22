f31b74ff12d3afaaf5a70a5dbe0a3ed3b3d23952
Andrew Schechtman-Rook <rook166@gmail.com>
2020-11-22 09:22:46 -0500

    fixed bug in predefined sample

---
+++ polling_simulator/sampling.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+
+from typing import Callable, Union, Tuple
+
+def post_stratified_sample_predefined():
+    """ie "I'm going to call 1000 people at random, and then we'll weight the responses
+    we get by assumed demographic representation
+    """
+
+
+def post_stratified_sample_guaranteed():
+    """ie "I'm going to call people at random until I get 1000 responses, and then we'll
+    weight the responses we get by assumed demographic representation
+    """
+
+
+def predefined_sample(n_sample, shuffled_electorate, max_num_attempts, *args, **kwargs):
+    """ie I'm going to call 1000 people at random, and just use those who answer"""
+    if n_sample > len(shuffled_electorate):
+        raise ValueError(f"number of samples ({n_sample}) greater than electorate ({len(shuffled_electorate)})")
+    people_called = shuffled_electorate.head(n_sample)
+    does_respond, num_attempts_required = _get_responses(
+        people_called["response_likelihood"], max_num_attempts
+    )
+    poll_responders = people_called[does_respond].reset_index(drop=True)
+    poll_responders["num_contact_attempts"] = num_attempts_required[does_respond]
+    poll_non_responders = people_called[does_respond == False].reset_index(drop=True)
+    poll_non_responders["num_contact_attempts"] = max_num_attempts
+    return poll_responders, poll_non_responders
+
+
+def guaranteed_sample(n_sample, shuffled_electorate, max_num_attempts, *args, **kwargs):
+    """ie I'm going to call people at random until I get 1000 responses"""
+    does_respond, num_attempts_required = _get_responses(
+        shuffled_electorate["response_likelihood"], max_num_attempts
+    )
+    # TODO: figure out how to add the info about number of attempts required
+    cumulative_responses = np.cumsum(does_respond)
+    if cumulative_responses[-1] < n_sample:
+        raise ValueError(
+            f"number of samples ({n_sample}) greater than number of poll responders ({cumulative_responses[-1]})"
+        )
+    people_contacted = shuffled_electorate[cumulative_responses <= n_sample]
+    does_respond = does_respond[cumulative_responses <= n_sample]
+    num_attempts_required = num_attempts_required[cumulative_responses <= n_sample]
+    poll_responders = people_contacted.loc[does_respond, :].reset_index(drop=True)
+    poll_responders["num_contact_attempts"] = num_attempts_required[does_respond]
+    poll_nonresponders = people_contacted.loc[does_respond == False, :].reset_index(drop=True)
+    poll_nonresponders["num_contact_attempts"] = max_num_attempts
+
+    return poll_responders, poll_nonresponders
+
+
+def pre_stratified_sample(
+        n_sample: int,
+        shuffled_electorate: pd.DataFrame,
+        max_num_attempts: int,
+        assumed_demographics,
+        sampling_strategy: Callable
+):
+    n_sample_per_demographic = [
+        round(n_sample * demographic.population_percentage)
+        for demographic in assumed_demographics
+    ]
+    population_per_demographic = [
+        shuffled_electorate.loc[
+            demographic.population_segmentation.segment(shuffled_electorate), :
+        ].copy(deep=True)
+        for demographic in assumed_demographics
+    ]
+    poll_responders = [
+        sampling_strategy(sample_size, demo_population, max_num_attempts)
+        for sample_size, demo_population in zip(n_sample_per_demographic, population_per_demographic)
+    ]
+    return pd.concat(poll_responders).reset_index(drop=True)
+
+
+def _get_responses(
+        response_likelihoods: Union[pd.Series, np.float64], max_num_attempts: int
+):
+    # -> Tuple[np.ndarray[np.bool_], np.ndarray[np.int64]]: # Not working, not sure why
+    """Figure out whether or not someone responds after N attempts. Return both whether or
+    not they responded but also the number of attempts made."""
+    if max_num_attempts < 1:
+        raise ValueError("max_num_attempts must be a positive integer")
+    realized_response_matrix = np.random.random((max_num_attempts, len(response_likelihoods)))
+    if isinstance(response_likelihoods, pd.Series):
+        response_likelihoods = response_likelihoods.values
+
+    did_respond_matrix = realized_response_matrix < response_likelihoods
+    did_respond = did_respond_matrix.any(axis=0)
+    num_attempts_required = did_respond_matrix.argmax(axis=0) + 1
+    num_attempts_required[did_respond == False] = -1
+    return did_respond, num_attempts_required
--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from polling_simulator import strategies
+from polling_simulator import sampling
 
 # @pytest.fixture(scope="function")
 # def electorate():
@@ -17,24 +17,24 @@ class TestPredefinedSample:
             "response_likelihood": np.ones(10)
         })
         with pytest.raises(ValueError):
-            strategies.predefined_sample(20, data, 1)
+            sampling.predefined_sample(20, data, 1)
 
     def test_returns_fewer_than_desired_when_response_rates_are_low(self):
         np.random.seed(123)
         data = pd.DataFrame({
             "response_likelihood": np.ones(20) * 0.5
         })
-        poll_responders, poll_non_responders = strategies.predefined_sample(10, data, 1)
+        poll_responders, poll_non_responders = sampling.predefined_sample(10, data, 1)
         assert len(poll_responders) < 10
         assert len(poll_responders) + len(poll_non_responders) == 10
 
     def test_reaches_more_people_when_makes_multiple_attempts(self):
         np.random.seed(123)
         data = pd.DataFrame({
-            "response_likelihood": np.ones(40) * 0.5
+            "response_likelihood": np.ones(40) * 0.1
         })
-        single_call_responders, _ = strategies.predefined_sample(20, data, 1)
-        multi_call_responders, non_responders = strategies.predefined_sample(20, data, 5)
+        single_call_responders, _ = sampling.predefined_sample(20, data, 1)
+        multi_call_responders, non_responders = sampling.predefined_sample(20, data, 5)
         assert len(multi_call_responders) > len(single_call_responders)
         assert len(multi_call_responders) < 20
         assert len(multi_call_responders) + len(non_responders) == 20
@@ -47,7 +47,7 @@ class TestGuaranteedSample:
             "response_likelihood": np.ones(10) * 0.05
         })
         with pytest.raises(ValueError):
-            strategies.guaranteed_sample(5, data, 1)
+            sampling.guaranteed_sample(5, data, 1)
 
     @pytest.mark.parametrize("num_people", [10, 50, 100, 500])
     def test_always_returns_the_asked_for_number_of_people(self, num_people):
@@ -55,7 +55,7 @@ class TestGuaranteedSample:
         data = pd.DataFrame({
             "response_likelihood": np.ones(10000) * 0.1
         })
-        responders, non_responders = strategies.guaranteed_sample(num_people, data, 1)
+        responders, non_responders = sampling.guaranteed_sample(num_people, data, 1)
         assert len(responders) == num_people
         assert len(non_responders) > 0
 
@@ -64,8 +64,8 @@ class TestGuaranteedSample:
         data = pd.DataFrame({
             "response_likelihood": np.ones(10000) * 0.1
         })
-        single_attempt_responders, single_attempt_non_responders = strategies.guaranteed_sample(100, data, 1)
-        multiple_attempt_responders, multiple_attempt_non_responders = strategies.guaranteed_sample(100, data, 5)
+        single_attempt_responders, single_attempt_non_responders = sampling.guaranteed_sample(100, data, 1)
+        multiple_attempt_responders, multiple_attempt_non_responders = sampling.guaranteed_sample(100, data, 5)
 
         assert len(single_attempt_responders) == len(multiple_attempt_responders)
         assert len(multiple_attempt_non_responders) < len(single_attempt_non_responders)
@@ -74,12 +74,12 @@ class TestGuaranteedSample:
 class TestInternalGetResponses:
     def test_fails_when_passed_zero_attempts(self):
         with pytest.raises(ValueError):
-            strategies._get_responses(None, 0)
+            sampling._get_responses(None, 0)
 
     def test_works_with_single_attempt(self):
         np.random.seed(123)
         response_likelihoods = np.ones(40) * 0.5
-        did_respond, num_attempts_required = strategies._get_responses(response_likelihoods, 1)
+        did_respond, num_attempts_required = sampling._get_responses(response_likelihoods, 1)
         assert np.sum(did_respond) < len(response_likelihoods)
         np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1]))
         assert np.min(num_attempts_required[did_respond]) == 1
@@ -87,7 +87,7 @@ class TestInternalGetResponses:
     def test_works_with_pandas_series(self):
         np.random.seed(123)
         response_likelihoods = pd.Series(np.ones(40) * 0.5)
-        did_respond, num_attempts_required = strategies._get_responses(response_likelihoods, 1)
+        did_respond, num_attempts_required = sampling._get_responses(response_likelihoods, 1)
         assert np.sum(did_respond) < len(response_likelihoods)
         np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1]))
         assert np.min(num_attempts_required[did_respond]) == 1
@@ -95,8 +95,8 @@ class TestInternalGetResponses:
     def test_works_with_multiple_attempts(self):
         np.random.seed(123)
         response_likelihoods = np.ones(40) * 0.25
-        did_respond_single, _ = strategies._get_responses(response_likelihoods, 1)
-        did_respond_multi, num_attempts_required = strategies._get_responses(response_likelihoods, 3)
+        did_respond_single, _ = sampling._get_responses(response_likelihoods, 1)
+        did_respond_multi, num_attempts_required = sampling._get_responses(response_likelihoods, 3)
         assert np.sum(did_respond_multi) > np.sum(did_respond_single)
         np.testing.assert_allclose(np.unique(num_attempts_required), np.array([-1, 1, 2, 3]))
         assert np.min(num_attempts_required[did_respond_multi]) == 1
