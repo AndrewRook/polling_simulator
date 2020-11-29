aba7f57173fb89b7cc85639b05ff804b97444851
Andrew Schechtman-Rook <rook166@gmail.com>
2020-11-29 09:26:00 -0500

    core stratified aggregation working

--- polling_simulator/aggregation.py
+++ polling_simulator/aggregation.py
@@ -1,21 +1,50 @@
+import numpy as np
 import pandas as pd
 
-
-def naive_aggregation(assumed_demographics, poll_responses, poll_nonresponses):
-    candidate_votes = poll_responses["candidate_preference"].value_counts()
-    return candidate_votes
+from copy import deepcopy
 
 
-def stratified_aggregation(assumed_demographics, poll_responses, poll_nonresponses):
-    stratified_votes = []
-    for demographic in assumed_demographics:
-        responses_in_demographic = demographic.population_segmentation.segment(poll_responses)
-        raw_votes = naive_aggregation(None, poll_responses[responses_in_demographic], None)
-        weight = demographic.population_percentage * demographic.turnout_likelihood
-        stratified_votes.append(raw_votes * weight)
-        # Don't need to worry about the denominator of the weights since we'll scale to
-        # percentages anyway
+def _no_turnout_weighting():
+    def _weighting(poll_responses):
+        return np.ones(len(poll_responses))
+    return _weighting
 
-    stratified_votes = pd.concat(stratified_votes).reset_index().groupby("index").sum()
-    return stratified_votes
 
+def naive_aggregation(turnout_weighting=_no_turnout_weighting()):
+
+    def _aggregation(poll_responses):
+        weighted_poll_responses = pd.DataFrame({
+            "candidate_preference": poll_responses["candidate_preference"].values,
+            "weight": turnout_weighting(poll_responses)
+        })
+        candidate_votes = weighted_poll_responses.groupby("candidate_preference")["weight"].sum()
+        return candidate_votes
+    return _aggregation
+
+
+def stratified_aggregation(assumed_demographics, turnout_weighting=_no_turnout_weighting()):
+    assumed_demographics = deepcopy(assumed_demographics)
+
+    def _aggregation(poll_responses):
+        stratified_votes = []
+        for demographic in assumed_demographics:
+            responses_in_demographic = demographic.population_segmentation.segment(poll_responses)
+            raw_votes = naive_aggregation(turnout_weighting)(poll_responses[responses_in_demographic])
+            population_weight = demographic.population_percentage
+            stratified_votes.append(
+                raw_votes # raw aggregation of polled people in the demographic
+                * population_weight # the size of the demographic
+                * len(poll_responses) # the relative size of the demographic in the poll compared to all poll responses
+                / responses_in_demographic.sum())
+            # Don't need to worry about the denominator of the weights since we'll scale to
+            # percentages anyway
+
+        stratified_votes = pd.concat(stratified_votes).reset_index().groupby("candidate_preference")["weight"].sum()
+        return stratified_votes
+    return _aggregation
+
+
+"""
+* likely voter weighting in aggregation
+   - probabilistic weighting based on either actual voting likelihood or demographic assumptions
+"""
--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 
-from polling_simulator import aggregation
+from functools import partial
+
+from polling_simulator import aggregation, Variable, Demographic
 
 
 class TestNaiveAggregation:
@@ -9,9 +12,44 @@ class TestNaiveAggregation:
         data = pd.DataFrame({
             "candidate_preference": ["a", "b", "a", "b", "a"]
         })
-        aggregate = aggregation.naive_aggregation(None, data, None).sort_values()
-        expected_aggregate = pd.Series([3, 2], index=["a", "b"], name="candidate_preference").sort_values()
-        pd.testing.assert_series_equal(aggregate, expected_aggregate)
+        aggregate = aggregation.naive_aggregation()(data).sort_values()
+        expected_aggregate = pd.Series([3.0, 2.0], index=["a", "b"]).sort_values()
+        pd.testing.assert_series_equal(aggregate, expected_aggregate, check_names=False)
+
 
 class TestStratifiedAggregation:
-    pass
+    def test_works_no_weighting(self):
+        gender = Variable("gender", partial(
+            np.random.choice, np.array(["M", "F"]), replace=True, p=np.array([0.5, 0.5])
+        ))
+
+        men = Demographic(0.5, 0.5, 0.5, {"a": 1}, (gender == "M"))
+        women = Demographic(0.5, 0.5, 1, {"b": 1}, (gender == "F"))
+
+        male_poll = pd.DataFrame({
+            "turnout_likelihood": np.ones(1000) * men.turnout_likelihood,
+            "response_likelihood": men.response_likelihood,
+            "candidate_preference": "a",
+            "gender": "M"
+        })
+        #male_poll.loc[:100, "candidate_preference"] = "b"
+        female_poll = pd.DataFrame({
+            "turnout_likelihood": np.ones(2000) * women.turnout_likelihood,
+            "response_likelihood": women.response_likelihood,
+            "candidate_preference": "b",
+            "gender": "F"
+        })
+        poll_results = pd.concat([male_poll, female_poll]).sample(frac=1)
+
+        naive_aggregate = aggregation.naive_aggregation()(poll_results).sort_values()
+        stratified_aggregate = aggregation.stratified_aggregation([men, women])(poll_results).sort_values()
+        pd.testing.assert_series_equal(
+            pd.Series([1000.0, 2000.0], index=["a", "b"]).sort_values(),
+            naive_aggregate,
+            check_names=False
+        )
+        pd.testing.assert_series_equal(
+            pd.Series([1500.0, 1500.0], index=["a", "b"]).sort_values(),
+            stratified_aggregate,
+            check_names=False
+        )
